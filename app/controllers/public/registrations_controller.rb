0a810832cd8ccf4d956394e1319ceaa4e365e78d
RikutoSuzuki <163372203+szki1990@users.noreply.github.com>
2024-05-23 07:39:42 +0000

    [Add]admin seed

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -1,8 +1,16 @@
 # frozen_string_literal: true
 
 class Public::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+   before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end 
+  
+  def after_sign_up_path_for(resource)
+    
+  end 
 
   # GET /resource/sign_up
   # def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Admin.create!(
+  email: "abc@abc",
+  password: "7777777"
+)
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
